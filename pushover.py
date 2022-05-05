8975cc9526be7cca0a33df23576acbd0388e7874
clutch70 <67942178+clutch70@users.noreply.github.com>
2022-05-05 04:36:17 -0500

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -1,12 +1,17 @@
 import requests
-
+from pathlib import import Path
 
 url = "https://api.pushover.net/1/messages.json"
 userAddress = "u1u24KYp2tAbk33xxQQ4S78rndVGi6"
 
 #Get an API key from pushover.net and create 
 #the file below with nothing else in it
-keyfile = open("pushover_api", "r")
+
+source_path = Path(__file__).resolve()
+source_dir = str(source_path.parent)
+file = source_dir + "/pushover_api"
+
+keyfile = open(file, "r")
 key = keyfile.read()
 keyfile.close
 
@@ -22,7 +27,7 @@ def run(title,message,priority):
     }
     
     
-   # print("key is",key)
+    print("key is",key)
     r = requests.post(url,data = postObjs)
     #print(r.text)
     
